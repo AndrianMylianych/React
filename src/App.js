75fceab557e0507f9ec7a8828034883d6037670e
AndrianMylianych <andrianmylianych@gmail.com>
2021-06-29 19:25:56 +0300

    here we have all posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import Users from "./components/Users/Users";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
     <div>
-      <Users/>
+      <Posts/>
     </div>
   );
 }
